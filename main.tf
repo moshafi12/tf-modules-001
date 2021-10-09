5adbf22b4be00f5f8b270bee76c65417398bbb84
Mo Shafi <mohammad.shafi@ihg.com>
2021-10-09 12:24:56 -0400

    third commit

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "default" {
 
 module "Instance" {
 	source        = "./modules/ec2"
-	name          = "lab-ec2-001"
+	name          = "lab-001"
 	vpc_id        = aws_vpc.default.id
 	cidr_block    = "172.31.0.0/16"
 	ami           = "ami-02354e95b39ca8dec"
